0b5aecc1e56d5a9c17c0a2116b268df8056c6d68
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-02-06 15:53:33 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,15 +3,6 @@ import sys
 import os
 import subprocess
 
-try:
-    assert len(sys.argv) == 2
-    version = ["-buggy", "-fix"][int(sys.argv[1])]
-except:
-    print(
-        "Please run 'python test_script 0' for testing the buggy-version and "
-        "'python test_script 1' for testing the fix-version.\nAborted...")
-    exit(1)
-
 interpreter_path = sys.executable
 print("Running at: ", interpreter_path)
 
@@ -20,14 +11,8 @@ assert tf.__version__ == "1.8.0"
 
 def get_target_dir():
     for x in os.listdir(os.path.dirname(os.path.abspath(__file__))):
-        if version in x:
-            return x
-    raise ValueError("No dir ends with %s!" % version)
-
-
-'''
-    Cautious: this subject can only be triggered with certain protobuf vetsion! See https://github.com/tensorflow/models/issues/1617
-'''
+        return x
+    
 print(
     "Cautious: this subject can only be triggered with certain protobuf vetsion! See https://github.com/tensorflow/models/issues/1617")
 subprocess.call(
