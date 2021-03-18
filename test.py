3655f7c3fa905f91566763ba5bf96d3b717636da
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-18 21:24:44 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,1 @@
+ptint("ValueError: Can not squeeze dim[1], expected a dimension of 1, got 28 for 'TestScope/resnet_v1_101/resnet_v1_101/SpatialSqueeze' (op: 'Squeeze') with input shapes: [4,28,28,2048].")
